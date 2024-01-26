512f75435e4045398c0458182ff9bf5740e6961e
danilapochekutov <danya270203@gamil.com>
2024-01-26 22:34:15 +0300

    add bild fils

--- src/components/pages/index.js
+++
@@ -1,9 +0,0 @@
-import MainPage from "../pages/MainPages";
-import ComicsPage from "../pages/ComicsPages";
-import Page404 from "./404";
-import SingleComicPage from "../pages/SingleComicPage/SingleComicPage";
-import SinglePage from "../pages/SinglePage";
-import SingleCharPage from "../pages/SingleCharPage/SingleCharPage";
-
-export {MainPage, ComicsPage, Page404, SingleComicPage, SingleCharPage, SinglePage};
- 
